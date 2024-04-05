38fd151f688db59d852600d749d50d4c9a62c6ff
Qui Nguyen <qui@gmail.com>
2024-04-04 21:37:16 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Users from './users/Users'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await axios.get('https://api.github.com/users')
+      // console.log(res.data);
+      setUsers(res.data);
+      setLoading(false)
+    }
+    getUsers()
+
+  }, [])
 
   return (
     <div className='App'>
       <Navbar />
       <div className="container">
-        <Users />
+        <Users users={users} />
       </div>
-
     </div>
   )
 }
--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,27 +1,6 @@
-import { useState } from "react";
 import UserItem from "./UserItem";
 
-const Users = () => {
-    const [users, setUsers] = useState([
-        {
-            id: 1,
-            login: "mojombo",
-            avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo",
-        },
-        {
-            id: 2,
-            login: "defunkt",
-            avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-            html_url: "https://github.com/defunkt",
-        },
-        {
-            id: 3,
-            login: "pjhyett",
-            avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-            html_url: "https://github.com/pjhyett",
-        },
-    ])
+const Users = ({ users }) => {
 
     const userStyle = {
         display: "grid",
