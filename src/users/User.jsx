7d6ef6380046f81275b6d04fdbb7647a933ba720
Qui Nguyen <qui@gmail.com>
2024-04-14 21:11:18 -0400

    first commit

--- src/users/User.jsx
+++ src/users/User.jsx
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import Spinner from '../components/layout/Spinner';
+import PropTypes from "prop-types";
+
+
 const User = ({ user, getUser, loading }) => {
     const { id } = useParams()
     const {
@@ -23,10 +27,49 @@ const User = ({ user, getUser, loading }) => {
     }, [])
 
 
+    if(loading) return <Spinner />
 
-    return <div>
-        {name}
-    </div>;
+    return <>
+        <Link to='/' className='btn btn-light'>Back To Search</Link>
+        Hireable: {' '} {hireable ? <i className='fas fa-check text-success' /> : <i className='fas fa-times-circle text-danger' />}
+        <div className="card grid-2">
+            <div className='all-center'>
+                <img src={avatar_url} alt="profile" className='round-img' style={{ width: '150px' }} />
+                <h1>{name}</h1>
+                <p>Location: {location}</p>
+            </div>
+            <div>
+                {bio && <>
+                    <h3>Bio</h3>
+                    <p>{bio}</p>
+                </>}
+                <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+                <ul>
+                    <li>{login && <>
+                        <strong>Username: </strong> {login}
+                    </>}</li>
+                    <li>{blog && <>
+                        <strong>Website: </strong> {blog}
+                    </>}</li>
+                </ul>
+            </div>
+
+        </div>
+        <div className="card text-center">
+            <div className="badge badge-primary">Followers: {followers}</div>
+            <div className="badge badge-success">Following: {following}</div>
+            <div className="badge badge-danger">Public repos: {public_repos}</div>
+            <div className="badge badge-dark">Public Grist: {public_gists}</div>
+        </div>
+    </>;
+
+    User.propTypes = {
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
+    }
+
+
 };
 
 export default User;
